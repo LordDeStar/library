4ddca95d61b11fd87eba537e36b1a554177591a5
LordDeStar <nikita.kalentiev@mail.ru>
2025-03-18 11:28:20 +0500

    lalalalal

--- src/components/MyBooks.jsx
+++ src/components/MyBooks.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 import { apiUrl } from "../config";
 export const MyBooks = ({ user, checkUser }) => {
     const [reservedBooks, setBooks] = useState([]);
+    const [onHands, setOnHands] = useState([]);
     const temp = async () => {
         console.log(user)
         const response = await fetch(apiUrl + '/Book/get/' + user.ticketNumber);
         if (response.ok) {
             const data = await response.json()
             setBooks(data.reservedBooks);
+            setOnHands(data.takenBooks);
         }
         else {
             alert(await response.statusText);
@@ -21,19 +23,38 @@ export const MyBooks = ({ user, checkUser }) => {
         temp();
     }, []);
     return (
-        <div className="list">
-            {reservedBooks.map((book, index) => {
-                return (
-                    <div className="card book" key={index}>
-                        <img className="card-img-top" src={book.imageUrl} alt={book.imageUrl} />
-                        <div className="card-body">
-                            <h5 className="card-title">{book.title}</h5>
-                            <p className="card-text">{book.publisher}</p>
-                            <p className="card-text">Статус: забронирвоанно</p>
+        <>
+            <div className="list" style={{ height: "fit-content" }}>
+                {reservedBooks.map((book, index) => {
+                    return (
+                        <div className="card book" key={index}>
+                            <img className="card-img-top" src={book.imageUrl} alt={book.imageUrl} />
+                            <div className="card-body">
+                                <h5 className="card-title">{book.title}</h5>
+                                <p className="card-text">{book.publisher}</p>
+                                <p className="card-text">Статус: забронирвоанно</p>
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
-        </div>
+                    )
+                })}
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div className="list">
+                {onHands.map((book, index) => {
+                    return (
+                        <div className="card book" key={index}>
+                            <img className="card-img-top" src={book.imageUrl} alt={book.imageUrl} />
+                            <div className="card-body">
+                                <h5 className="card-title">{book.title}</h5>
+                                <p className="card-text">{book.publisher}</p>
+                                <p className="card-text">Статус: на руках</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
     );
 }
